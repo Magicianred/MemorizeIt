0b6da0f4ac33a40d1d8d78a1c67df93366432aa2
Stacy <nastya.kholodova@gmail.com>
2013-06-19 19:54:17 -0400

    update home screen according to new interface of trainer

--- MemorizeIt.IOs/Screens/HomeScreen.cs
+++ MemorizeIt.IOs/Screens/HomeScreen.cs
@@ -16,148 +16,173 @@ using MemorizeIt.Model;
 
 namespace MemorizeIt.IOs.Screens {
 
-	/// <summary>
-	/// A UITableViewController that uses MonoTouch.Dialog - displays the list of Tasks
-	/// </summary>
-	public class HomeScreen : DialogViewController {
-		// 
-
-		
-		// MonoTouch.Dialog individual TaskDetails view (uses /AL/TaskDialog.cs wrapper class)
-		BindingContext context;
-		DialogViewController detailsScreen;
-		LoadingOverlay loadingOverlay;
-		private readonly IMemoryStorage store;
-		private readonly SimpleMemoryTrainer trainer;
-		public HomeScreen () : base (UITableViewStyle.Plain, null)
-		{
-			Initialize ();
-			this.store=new FileSystemMemoryStorage();
-			this.trainer = new SimpleMemoryTrainer (this.store);
-		}
-		
-		protected void Initialize()
-		{
-			NavigationItem.SetRightBarButtonItem (
-				new UIBarButtonItem ("Upadate",
-
-			    UIBarButtonItemStyle.Plain,
-			                     (sender, e) => { Upload(); }), false);
-			NavigationItem.SetLeftBarButtonItem (
-				new UIBarButtonItem ("Train",
-
-			                      UIBarButtonItemStyle.Plain,
-			                      (sender, e) => {
-				Train (); }), false);
-		}
-		protected void Upload(){
-
-			var dialod =new UIAlertView("Enter credentials", "Document path",null, "Cancel", null);
-
-			dialod.AlertViewStyle = UIAlertViewStyle.LoginAndPasswordInput;
-			dialod.AddButton ("Upload");
-
-			dialod.Show ();
-
-			dialod.Clicked += (sender,e) => {
-				if(e.ButtonIndex==0)
-					return;
-				loadingOverlay = new LoadingOverlay (UIScreen.MainScreen.Bounds);
-				View.Add (loadingOverlay);
-				var supplierParams=new string[]{"MemorizeIt", dialod.GetTextField (0).Text, dialod.GetTextField (1).Text};
-				Task.Factory.StartNew(()=>{
-					try{
-						var supplier = CreateSourceSupplier (supplierParams);
-						var data = supplier.Download ();
-						this.store.Store (data);
-				this.InvokeOnMainThread(()=>
-						                        PopulateTable ());
-					}catch(Exception downloadException){
-						this.InvokeOnMainThread(()=>
-						new UIAlertView("Error", downloadException.Message,null, "OK", null).Show());
-					}
-					this.InvokeOnMainThread(()=>
-					                        loadingOverlay.Hide ());
-				});
-			};
+    /// <summary>
+    /// A UITableViewController that uses MonoTouch.Dialog - displays the list of Tasks
+    /// </summary>
+    public class HomeScreen : DialogViewController
+    {
+        // 
 
 
-		}
+        // MonoTouch.Dialog individual TaskDetails view (uses /AL/TaskDialog.cs wrapper class)
+        private BindingContext context;
+        private DialogViewController detailsScreen;
+        private LoadingOverlay loadingOverlay;
+        private readonly IMemoryStorage store;
+        private readonly SimpleMemoryTrainer trainer;
 
-		protected void Train(){
-			ShowQuestion (trainer.GetQuestion());
+        public HomeScreen()
+            : base(UITableViewStyle.Plain, null)
+        {
+            Initialize();
+            this.store = new FileSystemMemoryStorage();
+            this.trainer = new SimpleMemoryTrainer(this.store);
+        }
 
-		}
+        protected void Initialize()
+        {
+            NavigationItem.SetRightBarButtonItem(
+                new UIBarButtonItem("Upadate",
 
-		protected IMemorySourceSupplier CreateSourceSupplier (params string[] supplierParameters)
-		{
-			return new GoogleMemorySourceSupplier (supplierParameters[0],supplierParameters[1],supplierParameters[2]);
-			/*return new SimpleMemorySourceSupplier (new MemoryItem[]{
-				new MemoryItem("q1","a1"),
-				new MemoryItem("q2","a2"),
-				new MemoryItem("q3","a3")
-			});*/
-		}
+                                    UIBarButtonItemStyle.Plain,
+                                    (sender, e) => Upload()), false);
+            NavigationItem.SetLeftBarButtonItem(
+                new UIBarButtonItem("Train",
 
-		protected void ShowQuestion(string s){
+                                    UIBarButtonItemStyle.Plain,
+                                    (sender, e) => Train()), false);
+        }
 
-			var dialod =new UIAlertView("I have a question for you", s,null, "Skip", null);
+        protected void Upload()
+        {
 
-			dialod.AlertViewStyle = UIAlertViewStyle.PlainTextInput;
-			dialod.AddButton ("Answer");
+            var dialod = new UIAlertView("Enter credentials", "Document path", null, "Cancel", null);
 
-			dialod.Show ();
+            dialod.AlertViewStyle = UIAlertViewStyle.LoginAndPasswordInput;
+            dialod.AddButton("Upload");
 
-			dialod.Clicked += (sender,e) => {
-				if (e.ButtonIndex == 0) {
-					trainer.Clear ();
-					return;
-				}
-				var answer=dialod.GetTextField (0).Text;
-				var result =trainer.Validate (answer);
-				PopulateTable ();
+            dialod.Show();
+
+            dialod.Clicked += (sender, e) =>
+                {
+                    if (e.ButtonIndex == 0)
+                        return;
+                    loadingOverlay = new LoadingOverlay(UIScreen.MainScreen.Bounds);
+                    View.Add(loadingOverlay);
+                    var supplierParams = new string[]
+                        {"MemorizeIt", dialod.GetTextField(0).Text, dialod.GetTextField(1).Text};
+
+                    Task.Factory.StartNew(() =>
+                        {
+                            try
+                            {
+                                var supplier = CreateSourceSupplier(supplierParams);
+                                var data = supplier.Download();
+                                this.store.Store(data);
+                                this.InvokeOnMainThread(PopulateTable);
+                            }
+                            catch (Exception downloadException)
+                            {
+                                this.InvokeOnMainThread(() =>
+                                                        new UIAlertView("Error", downloadException.Message, null, "OK",
+                                                                        null).Show());
+                            }
+                            this.InvokeOnMainThread(() =>
+                                                    loadingOverlay.Hide());
+                        });
+                };
 
 
+        }
 
-				if(result){
-					new UIAlertView("Well Done!", string.Format("'{0}' is correct answer",answer),null, "OK", null).Show();
+        protected void Train()
+        {
+            trainer.PickQuestion();
+            ShowQuestion(trainer.CurrentQuestion.Question);
 
-				}else{
-					new UIAlertView("Sorry", string.Format("'{0}' is correct answer on question '{2}'. Your answer was '{1}'",answer, trainer.GetAnswer(),s),null, "OK", null).Show();
-				}
-				trainer.Clear();
-				if(!trainer.IsQuestionsAvalible())
-					new UIAlertView("Well Done!", "You are done with all your questions",null, "OK", null).Show();
-			};
+        }
 
-		}
+        protected IMemorySourceSupplier CreateSourceSupplier(params string[] supplierParameters)
+        {
+            return new GoogleMemorySourceSupplier(supplierParameters[0], supplierParameters[1], supplierParameters[2]);
+            /*return new SimpleMemorySourceSupplier (new MemoryItem[]{
+                new MemoryItem("q1","a1"),
+                new MemoryItem("q2","a2"),
+                new MemoryItem("q3","a3")
+            });*/
+        }
 
-		public override void ViewWillAppear (bool animated)
-		{
-			base.ViewWillAppear (animated);
-			
-			// reload/refresh
-			PopulateTable();			
-		}
-		
-		protected void PopulateTable()
-		{
-			var memories = store.Items;
-			var items = new Section () {
+        protected void ShowQuestion(string s)
+        {
 
-				from t in memories
-				select (Element) new StringElement(string.Format("{1}({0})",t.SuccessCount,t.Values[0]),t.Values[1])
-			};
+            var dialod = new UIAlertView("I have a question for you", s, null, "Skip", null);
 
-			Root = new RootElement ("Memories") {
-				items
-			};
-		}
+            dialod.AlertViewStyle = UIAlertViewStyle.PlainTextInput;
+            dialod.AddButton("Answer");
 
-		public override Source CreateSizingSource (bool unevenRows)
-		{
-			return new EditingSource (this);
-		}
+            dialod.Show();
 
-	}
+            dialod.Clicked += (sender, e) =>
+                {
+                    if (e.ButtonIndex == 0)
+                    {
+                        trainer.Clear();
+                        return;
+                    }
+                    var answer = dialod.GetTextField(0).Text;
+                    var result = trainer.Validate(answer);
+                    PopulateTable();
+
+                    if (result)
+                    {
+                        new UIAlertView("Well Done!", string.Format("'{0}' is correct answer", answer), null, "OK", null)
+                            .Show();
+
+                    }
+                    else
+                    {
+                        new UIAlertView("Sorry",
+                                        string.Format(
+                                            "'{0}' is correct answer on question '{2}'. Your answer was '{1}'", answer,
+                                            trainer.CurrentQuestion.Answer, s), null, "OK", null).Show();
+                    }
+                    trainer.Clear();
+                    if (!trainer.IsQuestionsAvalible())
+                        new UIAlertView("Well Done!", "You are done with all your questions", null, "OK", null).Show();
+                };
+
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            // reload/refresh
+            PopulateTable();
+        }
+
+        protected void PopulateTable()
+        {
+            var memories = store.Items;
+            if (memories != null)
+            {
+                var items = new Section();
+                items.Add(from t in memories
+                          select
+                              (Element)
+                              new StringElement(string.Format("{1}({0})", t.SuccessCount, t.Values[0]), t.Values[1]));
+
+                Root = new RootElement("Memories")
+                    {
+                        items
+                    };
+            }
+        }
+
+        public override Source CreateSizingSource(bool unevenRows)
+        {
+            return new EditingSource(this);
+        }
+
+    }
 }
