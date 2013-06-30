2a5cbd068b3952a76a457742f5596ff8abf49bad
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-06-30 15:31:15 -0400

    icon to menu

--- MemorizeIt.IOs/Countrols/CustomSlideoutNavigationController.cs
+++ MemorizeIt.IOs/Countrols/CustomSlideoutNavigationController.cs
@@ -1,5 +1,6 @@
 using System;
 using MonoTouch.SlideoutNavigation;
+using MonoTouch.UIKit;
 
 namespace MemorizeIt.IOs
 {
@@ -11,7 +12,8 @@ namespace MemorizeIt.IOs
 		protected override MonoTouch.UIKit.UIBarButtonItem CreateMenuButton ()
 		{
 			var button= base.CreateMenuButton ();
-			button.Title="...";
+			button.Image = UIImage.FromFile ("menu-list.png");
+			button.Title="";
 			return button;
 		}
 	}
