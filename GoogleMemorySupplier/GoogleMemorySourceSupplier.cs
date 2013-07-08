553ad07661f296c745dbf8b3b7fbb1d0ab7acdcc
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-07-08 07:49:32 -0400

    doc name

--- GoogleMemorySupplier/GoogleMemorySourceSupplier.cs
+++ GoogleMemorySupplier/GoogleMemorySourceSupplier.cs
@@ -71,7 +71,7 @@ namespace GoogleMemorySupplier
                     new MemoryItem(new string[]
                         {((CellEntry) cellFeed.Entries[i]).Value, ((CellEntry) cellFeed.Entries[i + 1]).Value}));
             }
-            return new MemoryTable(applicationName, retval.ToArray());
+			return new MemoryTable(spreadsheetName, retval.ToArray());
         }
     }
 }
